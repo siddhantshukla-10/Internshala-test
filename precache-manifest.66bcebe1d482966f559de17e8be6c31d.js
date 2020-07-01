2db24125e85ba4cbef5b4ad762fdbb4c2dc68e29
siddhant <siddhant10shukla@gmail.com>
2020-07-01 18:35:51 +0530

    Updates

---
+++ precache-manifest.66bcebe1d482966f559de17e8be6c31d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4b1b3c905bae13402b9ec791e21f68",
+    "url": "/Internshala-test/index.html"
+  },
+  {
+    "revision": "3a984492318c1433a6bf",
+    "url": "/Internshala-test/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c54c3698dc11bf6781c5",
+    "url": "/Internshala-test/static/css/main.2f2cb679.chunk.css"
+  },
+  {
+    "revision": "3a984492318c1433a6bf",
+    "url": "/Internshala-test/static/js/2.67295f34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Internshala-test/static/js/2.67295f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54c3698dc11bf6781c5",
+    "url": "/Internshala-test/static/js/main.93b9c078.chunk.js"
+  },
+  {
+    "revision": "e620d59f0133d7b83bdf",
+    "url": "/Internshala-test/static/js/runtime-main.6726390a.js"
+  },
+  {
+    "revision": "c16c55d5ca478ea0f875e76d74c07269",
+    "url": "/Internshala-test/static/media/card1.c16c55d5.jpeg"
+  },
+  {
+    "revision": "f572d5bd2e071305ff4bb727d05bb403",
+    "url": "/Internshala-test/static/media/card2.f572d5bd.jpeg"
+  },
+  {
+    "revision": "7a71c16d61ef9f3bd058f2a6b4049a48",
+    "url": "/Internshala-test/static/media/card3.7a71c16d.jpeg"
+  }
+]);
